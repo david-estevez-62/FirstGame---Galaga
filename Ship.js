79de52287aa45b9821f754f7e3682e105e76aed5
Eric Hruska <e.hruska@aol.com>
2015-02-10 17:33:58 -0700

    Layout

---
+++ Ship.js
@@ -0,0 +1,106 @@
+Ship = function(gameClass) {
+	this.game = gameClass.game;
+    this.gameClass = gameClass;
+    this.sprite = null;
+    
+    this.bullets = null;
+    this.bulletTime = 0;
+    this.fireRate = 100;
+
+    this.nextFire = 0;
+
+    this.create(this.game);
+};
+
+
+Ship.prototype.create = function(game){
+	
+	this.sprite = this.game.add.sprite(this.game.width / 2-15, 550,'ship','fighterJet.png');
+	this.sprite.anchor.setTo(0.5,0.5);
+	this.sprite.enableBody = true;
+	
+	this.bullets = game.add.group();
+	this.bullets.enableBody = true;
+	this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+	
+	this.bullets.createMultiple(50,'bullet');
+	this.bullets.setAll('anchor.x', 0.5);
+    this.bullets.setAll('anchor.y', 1);
+	this.bullets.setAll('outOfBoundsKill', true);
+
+
+};
+
+Ship.prototype.update = function () {	
+	
+	 
+   //console.log("enemy"+this.gameClass.enemy1);
+  //if(this.gameClass.enemy1.die())
+   //var teste =  
+  // this.game.physics.collide(this.gameClass.enemy1.enemy, this.bullets, this.gameClass.enemy1.die, null, this);
+   //console.log(">>bullet "+this.bullets);
+};
+
+Ship.prototype.animate = function(){
+};
+
+Ship.prototype.stop = function(){
+	this.sprite.body.velocity.x = 0;
+	
+}
+
+Ship.prototype.move = function (direction) {
+
+	if( direction == "left" && this.sprite.x > 12){
+	    this.sprite.x -= 10;
+	}
+	        
+	else if (direction == "right" && this.sprite.x < this.game.width-12){
+	    this.sprite.x += 10;
+	 }
+	 
+};
+
+ 	
+Ship.prototype.shoot = function () {    
+    
+
+};
+
+Ship.prototype.destroyShoot = function (shoot) {
+	
+};
+
+Ship.prototype.die = function (spaceShip, asteroid) {
+
+};
+
+Ship.prototype.setSpriteShip = function(sprite){
+	this.sprite = sprite;
+}
+
+Ship.prototype.getSpriteShip = function(){
+	return this.sprite;
+}
+
+Ship.prototype.fire = function(){
+	if (this.game.time.now > this.bulletTime)
+    {
+        //  Grab the first bullet we can from the pool
+        bullet = this.bullets.getFirstExists(false);
+
+        if (bullet)
+        {
+            //  And fire it
+            bullet.reset(this.sprite.x, this.sprite.y);//posicáo de saida do tiro
+            bullet.body.velocity.y = -800; //velocidade do projetil
+            this.bulletTime = game.time.now + 200;
+        }
+    }
+
+}
+
+Ship.prototype.die = function(bullet,ship){
+	bullet.kill();
+	ship.kill();
+}
--- main.js
+++ main.js
@@ -1,23 +1,57 @@
+// Game = function() {
+// 	this.cursors = XV.game.input.keyboard.createCursorKeys();
 
-  
+//      	this.wasd = {
+//                 up: XV.game.input.keyboard.addKey(Phaser.Keyboard.W),
+//                 down: XV.game.input.keyboard.addKey(Phaser.Keyboard.S),
+//                 left: XV.game.input.keyboard.addKey(Phaser.Keyboard.A),
+//                 right: XV.game.input.keyboard.addKey(Phaser.Keyboard.D),
+//             };
+// } 
 var fighterJet;
 
-var game= new Phaser.Game(
-	800, 600, Phaser.AUTO, '',
+var game = new Phaser.Game(
+	1000, 750, Phaser.AUTO, '',
 	{ preload: preload, create: create, update: update}
 );
 
 function preload() {
-	game.load.image('jet', 'fighterJet.png');
+	game.load.image('ship', 'fighterJet.png');
 }
 
 function create() {
-	fighterJet = game.add.sprite(game.world.centerX, 0, 'jet');
+	fighterJet = game.add.sprite(game.world.centerX, 600, 'ship');
 	
 }
 
 
 function update() {
 
-}
+	 fighterJet.x = game.input.x;
+	 cursors = game.input.keyboard.createCursorKeys();
+        var fighterJetHalfWidth = fighterJet.width / 2;
+        
+        if (fighterJet.x < fighterJetHalfWidth) {
+            fighterJet.x = fighterJetHalfWidth;
+        } else if (fighterJet.x > game.width - fighterJetHalfWidth) {
+            fighterJet.x = game.width - fighterJetHalfWidth;
+        } 
 
+// cursors = game.input.keyboard.createCursorKeys();
+  // 		if (cursors.left.isDown) {
+		// 	fighterJet.velocity.x = -200;
+		// } else if (cursors.right.isDown) {
+		// 	fighterJet.velocity.x = 200;
+		// }
+
+ }
+
+
+
+
+
+
+
+// fighterJet.onkeydown=function(){
+//   			if 
+//   		};
