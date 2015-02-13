a5d62e1048660261c6e9fc38e7e582375b92aa51
ehruska <ejh81491@gmail.com>
2015-02-12 23:50:50 -0700

    Galaga alpha

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-// Game = function() {
-// 	
 
-//      	this.wasd = {
-//                 up: XV.game.input.keyboard.addKey(Phaser.Keyboard.W),
-//                 down: XV.game.input.keyboard.addKey(Phaser.Keyboard.S),
-//                 left: XV.game.input.keyboard.addKey(Phaser.Keyboard.A),
-//                 right: XV.game.input.keyboard.addKey(Phaser.Keyboard.D),
-//             };
-// } 
 
 var ship;
 var bullets;
@@ -65,180 +56,134 @@ function preload() {
 
 function create() {
 
-			starfield = game.add.tileSprite(game.world.centerX / 100, 0, 1050, 750, 'starfield');
+		starfield = game.add.tileSprite(game.world.centerX / 100, 0, 1050, 750, 'starfield');
+		
+		logo = game.add.sprite(40, 65, 'logo');
+
+
+
+		game.cursors = game.input.keyboard.createCursorKeys();
+		
+
+		var moveLeft = function () {
+			ship.isMoving = "left"
+		}
+		var moveRight = function () {
+			ship.isMoving = "right";
+		}
+
+		var stopMoving1 = function() {
+			ship.isMoving = false;
+		}
+		var stopMoving2 = function() {
+			ship.isMoving = false;
+		}
+
+
+
+		game.cursors.left.onDown.add(moveLeft);
+	    game.cursors.right.onDown.add(moveRight);
+	    game.cursors.left.onUp.add(stopMoving1);
+		game.cursors.right.onUp.add(stopMoving2);
+
+
+		bullets = game.add.group();
+		var shoot = function() {
+			var bullet1 = bullets.create(ship.x+25,ship.y+50, 'bullet')
+			var bullet2 = bullets.create(ship.x+55,ship.y+50, 'bullet')
+			game.physics.arcade.enable([bullet1, bullet2]);
+			bullet1.body.velocity.y = -800; 
+			bullet2.body.velocity.y = -800; 
+
+			sound1 = game.sound.play('laser');
+
+
+		}
+
+		fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		fireButton.onDown.add(shoot);
+
+
+
+
+
 			
-			logo = game.add.sprite(40, 65, 'logo');
-
-			game.cursors = game.input.keyboard.createCursorKeys();
-			
-
-			var moveLeft = function () {
-				ship.isMoving = "left"
-			}
-			var moveRight = function () {
-				ship.isMoving = "right";
-			}
-
-			var stopMoving1 = function() {
-				ship.isMoving = false;
-			}
-			var stopMoving2 = function() {
-				ship.isMoving = false;
-			}
-
-
-
-			game.cursors.left.onDown.add(moveLeft);
-		    game.cursors.right.onDown.add(moveRight);
-		    game.cursors.left.onUp.add(stopMoving1);
-			game.cursors.right.onUp.add(stopMoving2);
-
-
-			bullets = game.add.group();
-			var shoot = function() {
-				var bullet1 = bullets.create(ship.x+25,ship.y+50, 'bullet')
-				var bullet2 = bullets.create(ship.x+55,ship.y+50, 'bullet')
-				game.physics.arcade.enable([bullet1, bullet2]);
-				bullet1.body.velocity.y = -800; 
-				bullet2.body.velocity.y = -800; 
-
-				sound1 = game.sound.play('laser');
-
-
-			}
-
-			fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-			fireButton.onDown.add(shoot);
 
 
 
 
-
-			aliens1 = game.add.group();
-            aliens1.visible = true;
-
+		aliens1 = game.add.group();
+        aliens1.visible = true;
 
 
-			function createAliens() { //aliens 3 verde
-                for (var y = 0; y <= 2; y++)
+
+		function createAliens() { //aliens 3 verde
+            for (var y = 0; y <= 2; y++)
+            {
+                for (var x = 0; x < 8; x++)
                 {
-                    for (var x = 0; x < 8; x++)
-                    {
-                        //   var aliens = aliens1.create(x * 50 + game.world.centerX / 2 + 35, y * 30, 'aliens1');
-                        var aliens = aliens1.create(x*30+game.world.centerX / 2, y*30, 'aliens1');
-                        aliens.anchor.setTo(0.5, 0.5);
-                        game.physics.arcade.enable(aliens);
-                        // mov_alien(aliens1), x * 50 + game.world.centerX / 2 + 35, y * 30);
-                        aliens.animations.add('fly3', [0, 1], 2, true);
-                        aliens.play('fly3');
-                    }
+                    //   var aliens = aliens1.create(x * 50 + game.world.centerX / 2 + 35, y * 30, 'aliens1');
+                    var aliens = aliens1.create(x*30+game.world.centerX / 2, y*30, 'aliens1');
+                    aliens.anchor.setTo(0.5, 0.5);
+                    game.physics.arcade.enable(aliens);
+                    // mov_alien(aliens1), x * 50 + game.world.centerX / 2 + 35, y * 30);
+                    aliens.animations.add('fly3', [0, 1], 2, true);
+                    aliens.play('fly3');
                 }
-                aliens1.x = 100;
-                aliens1.y = 50;
-                //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-                var tween = game.add.tween(aliens1).to({x: 200}, 2000, Phaser.Easing.Circular.InOut, true, 9400, 100, true);
-                var tween2 = game.add.tween(aliens1).to({y: 510}, 2000, Phaser.Easing.Circular.InOut, true, 9200, 100, true);
-                //  When the tween completes it calls descend, before looping again
-                tween.onComplete.add(descend, this);
             }
+            aliens1.x = 100;
+            aliens1.y = 50;
+            //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
+            var tween = game.add.tween(aliens1).to({x: 200}, 2000, Phaser.Easing.Circular.InOut, true, 9400, 100, true);
+            var tween2 = game.add.tween(aliens1).to({y: 510}, 2000, Phaser.Easing.Circular.InOut, true, 9200, 100, true);
+            //  When the tween completes it calls descend, before looping again
+            tween.onComplete.add(descend, this);
+        }
 
 
 
-			// // The enemy's bullets
-			// 	enemyBullets = game.add.group();
-			// 	enemyBullets.enableBody = true;
-			// 	enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
-			// 	enemyBullets.createMultiple(30, 'enemyBullet');
-			// 	enemyBullets.setAll('anchor.x', 0.5);
-			// 	enemyBullets.setAll('anchor.y', 1);
-			// 	enemyBullets.setAll('outOfBoundsKill', true);
-			// 	enemyBullets.setAll('checkWorldBounds', true);
-
-
-			// 	aliens = game.add.group();
-			// 	aliens.enableBody = true;
-			// 	aliens.physicsBodyType = Phaser.Physics.ARCADE;
-
-			// 	createAliens();
-
-			// 	//The score
-			// 	scoreString = "Score: ";
-			// 	scoretext = game.add.text(10,10, scoreString + score, { font: '34px Arial', fill: '#fff' });
-
-			// 	//Lives
-			// 	lives = game.add.group();
-			// 	game.add.text(game.world.width - 100, 10, 'Lives: ', {font: '34px Arial', fill: '#fff'});
-
-			// 	stateText = game.add.text(game.world.centerX, game.world.centerY,"",{ font: '84px Arial', fill: '#fff'});
-			// 	stateText.anchor,setTo(0.5,0.5);
-			// 	stateText.visible = false;
-
-			// 	for (var i = 0; i < 3; i++) {
-			// 		var ship = lives.create(game.world.width - 100 + (30 * i), 60, 'ship');
-			// 		ship.anchor.setTo(0.5, 0.5);
-			// 		ship.angle = 90;
-			// 		ship.alpha = 0.4;
-			// 	}
-
-			// 	//  An explosion pool
-			// 	explosions = game.add.group();
-			// 	explosions.createMultiple(30, 'kaboom');
-			// 	explosions.forEach(setupInvader, this); 
-
-
-			
-
-
-	  
-			// var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-
-			// 	//  When the tween loops it calls descend
-			// 	tween.onLoop.add(descend, this);
-			// }
+		
 
 
 
-			function descend() {
-				aliens.y += 10;
-			}
+		function descend() {
+			aliens.y += 10;
+		}
 
 
-		     var playGame = function () {
+	     var playGame = function () {
 
-		     	game.hasStarted = true;
-		     	logo.destroy();
-		     	ship = game.add.sprite(game.world.centerX, 600, 'ship');
-		     	ship.isMoving = false;
+	     	game.hasStarted = true;
+	     	logo.destroy();
+	     	ship = game.add.sprite(game.world.centerX, 600, 'ship');
+	     	ship.isMoving = false;
 
-		     	
-		     		sound2 = game.sound.play('thruster', 2, true);
+	     	
+	     	sound2 = game.sound.play('thruster', 2, true);
 
-		     	createAliens();
+	     	createAliens();
      	
-     	
-
-     }
+     	}
 
 
 
-     	logo.inputEnabled = true;
+     logo.inputEnabled = true;
 
-        logo.input.useHandCursor = true;
+     logo.input.useHandCursor = true;
 
-        logo.events.onInputDown.add(playGame, this);
+     logo.events.onInputDown.add(playGame, this);
 
 
      // buttonNewGame = game.add.button(game.world.centerX - 50, 440, 'button2', actionOnClickVoltar, this, 2, 1, 0);
      // buttonNewGame.visible = false;
 
-     	// buttonCredits = game.add.button(game.world.centerX - 95, 340, 'button2', actionOnClickCreditos, this, 2, 1, 0);
-
-
-
+     // buttonCredits = game.add.button(game.world.centerX - 95, 340, 'button2', actionOnClickCreditos, this, 2, 1, 0);
 
 
 
 }
+
+
 
 function update() {
 
@@ -268,31 +213,26 @@ function update() {
 	////////////////////////////
 	// ship.x = game.input.x;
 	 
- //        var shipHalfWidth = ship.width / 2;
+ 	//     var shipHalfWidth = ship.width / 2;
         
- //        if (ship.x < shipHalfWidth) {
- //            ship.x = shipHalfWidth;
- //        } else if (ship.x > game.width - shipHalfWidth) {
- //            ship.x = game.width - shipHalfWidth;
- //        } 
+ 	//     if (ship.x < shipHalfWidth) {
+ 	//         ship.x = shipHalfWidth;
+ 	//     } else if (ship.x > game.width - shipHalfWidth) {
+ 	//         ship.x = game.width - shipHalfWidth;
+ 	//     } 
 
 
- 	// 	if (game.time.now > firingTimer) {
-		// 	enemyFires();
-		// }
-
-
- 		game.physics.arcade.overlap(bullets, aliens1, collisionHandler, null, this);
-		// game.physics.arcade.overlap(enemyBullets, ship, enemyHitsPlayer, null, this);
+ 
+ 	game.physics.arcade.overlap(bullets, aliens1, collisionHandler, null, this);
+	// game.physics.arcade.overlap(enemyBullets, ship, enemyHitsPlayer, null, this);
 		
 
 
+	// 	if (game.time.now > firingTimer) {
+		// 	enemyFires();
+		// }
+
  }
-
-
-
-
-
 
 
 
@@ -302,24 +242,113 @@ function collisionHandler (bullet, aliens) {
 	aliens.kill();
 	sound3 = game.sound.play('explosion');
 
-	// score += 20;
-	// scoreText.text = scoreString + score;
+						// score += 20;
+						// scoreText.text = scoreString + score;
 
-	// var explosion = explosions.getFirstExists(false);
-	// explosion.reset(alien.body.x, alien.body.y);
-	// explosion.play('kaboom', 30, false, true);
+						// var explosion = explosions.getFirstExists(false);
+						// explosion.reset(alien.body.x, alien.body.y);
+						// explosion.play('kaboom', 30, false, true);
 
-	// if(aliens.countLiving() === 0) {
-	// 	score +=1000;
-	// 	scoreText = scoreString + score;
-	// 	enemyBullets.callAll('kill', this);
-	// 	stateText.text = ' Speed it up! \n Click to restart.'
-	// 	stateText.visible = true;
+						// if(aliens.countLiving() === 0) {
+						// 	score +=1000;
+						// 	scoreText = scoreString + score;
+						// 	// enemyBullets.callAll('kill', this);
+						// 	stateText.text = ' Speed it up! \n Click to restart.'
+						// 	stateText.visible = true;
 
-	// 	game.input.onTap.addOnce(restart, this);
-	// }
+						// 	game.input.onTap.addOnce(restart, this);
+						// }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// //Lives
+	// 		lives = game.add.group();
+	// 		game.add.text(game.world.width - 100, 10, 'Lives: ', {font: '34px Arial', fill: '#fff'});
+
+	// 		stateText = game.add.text(game.world.centerX, game.world.centerY,"",{ font: '84px Arial', fill: '#fff'});
+	// 		stateText.anchor,setTo(0.5,0.5);
+	// 		stateText.visible = false;
+
+	// 		for (var i = 0; i < 3; i++) {
+	// 			var ship = lives.create(game.world.width - 100 + (30 * i), 60, 'ship');
+	// 			ship.anchor.setTo(0.5, 0.5);
+	// 			ship.angle = 90;
+	// 			ship.alpha = 0.4;
+	// 		}
+
+
+	// // The enemy's bullets
+			// 	enemyBullets = game.add.group();
+			// 	enemyBullets.enableBody = true;
+			// 	enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
+			// 	enemyBullets.createMultiple(30, 'enemyBullet');
+			// 	enemyBullets.setAll('anchor.x', 0.5);
+			// 	enemyBullets.setAll('anchor.y', 1);
+			// 	enemyBullets.setAll('outOfBoundsKill', true);
+			// 	enemyBullets.setAll('checkWorldBounds', true);
+
+			
+	  
+			// var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+
+			// 	//  When the tween loops it calls descend
+			// 	tween.onLoop.add(descend, this);
+			// }
+
+	// function restart () {
+	// 	lives.callAll('revive');
+	// 	aliens.removeAll();
+	// 	createAliens();
+
+	// 	ship.revive();
+	// 	stateText.visible = false;
+	// }
+
+
+
+
 
 
 
@@ -376,24 +405,10 @@ function collisionHandler (bullet, aliens) {
 // 		}
 // 	}
 
-
-
-function restart () {
-	lives.callAll('revive');
-	aliens.removeAll();
-	createAliens();
-
-	ship.revive();
-	stateText.visible = false;
-}
-
-
-
-
-
-
-
 	
+///////////////////////////////
+// Intro, Credits, Reset Pgs //
+///////////////////////////////
 
 // function actionOnClick() {
 //                 logo.visible = false;
@@ -415,75 +430,21 @@ function restart () {
 // }
 
 
-	  
-	 
-        
-        
-        
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/////////////
+// Cursors //
+/////////////
+
+// Game = function() {
+// 	
+//      	this.wasd = {
+//                 up: XV.game.input.keyboard.addKey(Phaser.Keyboard.W),
+//                 down: XV.game.input.keyboard.addKey(Phaser.Keyboard.S),
+//                 left: XV.game.input.keyboard.addKey(Phaser.Keyboard.A),
+//                 right: XV.game.input.keyboard.addKey(Phaser.Keyboard.D),
+//             };
+// } 	  
 
 // cursors = game.input.keyboard.createCursorKeys();
   // 		if (cursors.left.isDown) {
@@ -491,15 +452,6 @@ function restart () {
 		// } else if (cursors.right.isDown) {
 		// 	fighterJet.velocity.x = 200;
 		// }
-
-
-   
-
-
-
-
-
-
 
 
 // fighterJet.onkeydown=function(){
