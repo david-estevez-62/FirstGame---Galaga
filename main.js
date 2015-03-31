43a9e70791d052a1ef2f4c6d006b89542a5e5bfb
Eric <ejh81491@gmail.com>
2015-03-31 17:04:00 -0600

    Added scoring

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 var ship;
 var bullets;
 var bulletTime = 0;
@@ -105,13 +103,12 @@ function create() {
 
 
 
+		//  The score
+		scoreString = 'Score : ';
+		scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' });
 
 
 			
-
-
-
-
 		aliens1 = game.add.group();
         aliens1.visible = true;
 
@@ -242,218 +239,24 @@ function collisionHandler (bullet, aliens) {
 	aliens.kill();
 	sound3 = game.sound.play('explosion');
 
-						// score += 20;
-						// scoreText.text = scoreString + score;
+	//  Increase the score
+	score += 10;
+	scoreText.text = scoreString + score;
 
-						// var explosion = explosions.getFirstExists(false);
-						// explosion.reset(alien.body.x, alien.body.y);
-						// explosion.play('kaboom', 30, false, true);
+		// var explosion = explosions.getFirstExists(false);
+		// explosion.reset(alien.body.x, alien.body.y);
+		// explosion.play('kaboom', 30, false, true);
 
-						// if(aliens.countLiving() === 0) {
-						// 	score +=1000;
-						// 	scoreText = scoreString + score;
-						// 	// enemyBullets.callAll('kill', this);
-						// 	stateText.text = ' Speed it up! \n Click to restart.'
-						// 	stateText.visible = true;
+		// if(aliens.countLiving() === 0) {
+		// 	score +=1000;
+		// 	scoreText = scoreString + score;
+		// 	// enemyBullets.callAll('kill', this);
+		// 	stateText.text = ' Speed it up! \n Click to restart.'
+		// 	stateText.visible = true;
 
-						// 	game.input.onTap.addOnce(restart, this);
-						// }
+		// 	game.input.onTap.addOnce(restart, this);
+		// }
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// //Lives
-	// 		lives = game.add.group();
-	// 		game.add.text(game.world.width - 100, 10, 'Lives: ', {font: '34px Arial', fill: '#fff'});
-
-	// 		stateText = game.add.text(game.world.centerX, game.world.centerY,"",{ font: '84px Arial', fill: '#fff'});
-	// 		stateText.anchor,setTo(0.5,0.5);
-	// 		stateText.visible = false;
-
-	// 		for (var i = 0; i < 3; i++) {
-	// 			var ship = lives.create(game.world.width - 100 + (30 * i), 60, 'ship');
-	// 			ship.anchor.setTo(0.5, 0.5);
-	// 			ship.angle = 90;
-	// 			ship.alpha = 0.4;
-	// 		}
-
-
-	// // The enemy's bullets
-			// 	enemyBullets = game.add.group();
-			// 	enemyBullets.enableBody = true;
-			// 	enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
-			// 	enemyBullets.createMultiple(30, 'enemyBullet');
-			// 	enemyBullets.setAll('anchor.x', 0.5);
-			// 	enemyBullets.setAll('anchor.y', 1);
-			// 	enemyBullets.setAll('outOfBoundsKill', true);
-			// 	enemyBullets.setAll('checkWorldBounds', true);
-
-			
-	  
-			// var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-
-			// 	//  When the tween loops it calls descend
-			// 	tween.onLoop.add(descend, this);
-			// }
-
-	// function restart () {
-	// 	lives.callAll('revive');
-	// 	aliens.removeAll();
-	// 	createAliens();
-
-	// 	ship.revive();
-	// 	stateText.visible = false;
-	// }
-
-
-
-
-
-
-
-// function enemyHitsPlayer (ship, bullet) {
-// 	bullet.kill();
-
-// 	var live = lives.getFirstAlive();
-
-// 	if (live){
-// 		live.kill();
-// 	}
-
-// 	var explosions = explosions.getFirstExists(false);
-// 	explosion.reset(ship.body.x, ship.body.y);
-// 	explosion.play('kaboom', 30, false, true);
-
-// 	if(lives.countLiving() < 1 ) {
-// 		ship.kill();
-// 		enemyBullets.callAll('kill');
-
-// 		stateText.text="GAME OVER \n Click to restart";
-// 		stateText.visible = true;
-
-// 		game.inpit.onTap.addOnce(restart, this);
-// 	}
-
-// }
-
-
-// // Create a function where if the enemy is alive then they can shoot
-// function enemyFires () {
-
-// 		//  Grab the first bullet we can from the pool
-// 		enemyBullet = enemyBullets.getFirstExists(false);
-
-// 		livingEnemies.length=0;
-
-// 		aliens.forEachAlive(function(alien){
-
-// 			// put every living enemy in an array
-// 			livingEnemies.push(alien);
-// 		});
-
-// 		if (enemyBullet && livingEnemies.length > 0) {
-// 			var random=game.rnd.integerInRange(0,livingEnemies.length-1);
-
-// 			// randomly select one of them
-// 			var shooter=livingEnemies[random];
-// 			// And fire the bullet from this enemy
-// 			enemyBullet.reset(shooter.body.x, shooter.body.y);
-
-// 			game.physics.arcade.moveToObject(enemyBullet,ship,120);
-// 			firingTimer = game.time.now + 2000;
-// 		}
-// 	}
-
-	
-///////////////////////////////
-// Intro, Credits, Reset Pgs //
-///////////////////////////////
-
-// function actionOnClick() {
-//                 logo.visible = false;
-//                 ship.visible = true;
-//                 buttonInitiate.visible = false;
-//                 buttonCredits.visible = false;
-//     }
-// function actionOnClickCreditos() {
-//                 logo.visible = false;
-//                 buttonInitiate.visible = false;
-//                 buttonNewGame.visible = true;
-//                 buttonCredits.visible = false;
-//     }
-// function actionOnClickVoltar() {
-//                 buttonInitiate.visible = true;
-//                 logo.visible = true;
-//                 buttonCredits.visible = true;
-//                 buttonNewGame.visible = false;
-// }
-
-
-
-
-/////////////
-// Cursors //
-/////////////
-
-// Game = function() {
-// 	
-//      	this.wasd = {
-//                 up: XV.game.input.keyboard.addKey(Phaser.Keyboard.W),
-//                 down: XV.game.input.keyboard.addKey(Phaser.Keyboard.S),
-//                 left: XV.game.input.keyboard.addKey(Phaser.Keyboard.A),
-//                 right: XV.game.input.keyboard.addKey(Phaser.Keyboard.D),
-//             };
-// } 	  
-
-// cursors = game.input.keyboard.createCursorKeys();
-  // 		if (cursors.left.isDown) {
-		// 	fighterJet.velocity.x = -200;
-		// } else if (cursors.right.isDown) {
-		// 	fighterJet.velocity.x = 200;
-		// }
-
-
-// fighterJet.onkeydown=function(){
-//   			if 
-//   		};
